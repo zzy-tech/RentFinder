ec6bcb9b66ad3d14ad1f2ddde497531f679c8283
Zhang Zhiyong <zzylikemj@me.com>
2024-02-19 20:29:41 +0900

    first commit

---
+++ src/main/java/com/rentfinder/RentfinderApplication.java
@@ -0,0 +1,13 @@
+package com.rentfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentfinderApplication.class, args);
+	}
+
+}
