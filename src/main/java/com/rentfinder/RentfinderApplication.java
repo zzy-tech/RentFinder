d7365c5a7582d8d4387a6c48984db81677a3b12d
Zhang Zhiyong <zzylikemj@me.com>
2024-02-19 22:50:51 +0900

    Add Checkstyle rules

--- src/main/java/com/rentfinder/RentfinderApplication.java
+++ src/main/java/com/rentfinder/RentfinderApplication.java
@@ -3,11 +3,14 @@ package com.rentfinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main entry point.
+ */
 @SpringBootApplication
 public class RentfinderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RentfinderApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RentfinderApplication.class, args);
+  }
 
 }
